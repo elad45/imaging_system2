a732d165f5135442eed9375a50171757e1931902
elad45 <92677845+elad45@users.noreply.github.com>
2023-06-18 14:32:18 -0700

    movements

--- movements/moveWithDAQ.m
+++ movements/moveWithDAQ.m
@@ -4,8 +4,8 @@ function velocity = moveWithDAQ(vr)
      %  velocity = [0 0 0 0];
      %   return
     %end
-    counter = 0;
-    direction = runDirectiongpt(dataFromDAQ);
+    numOfSlits = 0; % slits in the rotary encoder
+    direction = runDirection(dataFromDAQ);
 %     direction = 1;
     Acol = dataFromDAQ(:,1);
     Bcol = dataFromDAQ(:,2);
@@ -15,23 +15,20 @@ function velocity = moveWithDAQ(vr)
     length = numel(Bcol);
     for i = 2:length
         tmp = Bcol(i-1) + Bcol(i);
-        if tmp>3 && tmp < 6
-            counter = counter +1;
+        if tmp>3 && tmp < 6 % Volt bounderies - expected to be 4 when activated
+            numOfSlits = numOfSlits +1;
         end
     end
     % extract the speed from the encoder
-    angle = (counter/1024)*360; % 1024 slits in the encoder
-    angularSpeed = angle ./ double(vr.timeOfSample);
-%     speed = angularSpeed*0.09525; % radius of 9.525 cm for the wheel.
-    %diff = data - lastDataFromDAQ;
-    
-    speed = counter*5.8445; % cm/s
-    % 10000 = 100 for 10 ms to sec * 100 for cm to meter => velocity in m\s
+    linearSpeedPerSlit = 5.8445;
+    speed = numOfSlits*linearSpeedPerSlit; % cm/s 
     scaling = 1;
-%     velocity = [0 speed*scaling*direction 0 0];
-      velocity = [0 40 0 0];
+    
+    %velocity = [0 speed*scaling*direction 0 0];
+     velocity = [0 40 0 0];
     %calculate distance- 1024 slots / 360 deg = 2.844 slots per deg
-    rotationAngle = counter*2.844;
+    slitsPerDeg = 2.844;
+    rotationAngle = numOfSlits*slitsPerDeg;
     
     vr = logData(vr, Acol, Bcol, lickPortCol, velocity, timestampCol);
 

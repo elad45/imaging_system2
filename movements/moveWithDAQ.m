61c66215aa227c77320d36f1c815f13a12d28013
elad45 <92677845+elad45@users.noreply.github.com>
2023-06-25 18:35:33 +0300

    Add files via upload

--- movements/moveWithDAQ.m
+++ movements/moveWithDAQ.m
@@ -4,8 +4,6 @@ function velocity = moveWithDAQ(vr)
     velocity = [0 vr.avgVelocity 0 0];
     %velocity = [0 40 0 0];
     
-    %vr = logData(vr, Acol, Bcol, lickPortCol, velocity, timestampCol);
-
     if (vr.position(2)>=vr.endOftheRoad) 
         velocity(2) = 0;
     end

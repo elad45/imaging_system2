684a39f78edfbd1fbcc53a7fd73c8565cf6ba104
itamar-st <81354824+itamar-st@users.noreply.github.com>
2023-12-04 20:31:25 +0200

    Add files via upload

--- functions/one_room_reward_by_init_sound/endOfTraceProcedure2.m
+++ functions/one_room_reward_by_init_sound/endOfTraceProcedure2.m
@@ -1,7 +1,6 @@
 function vr = endOfTraceProcedure2(vr)
     global timeUntilCoolOffRoom;
     global dataFromDAQ;
-   
     %1 for switching to black room and 2 for jumping back to start
     timestampCol = dataFromDAQ(:,4);
     vr = stopSound(vr);
@@ -9,7 +8,6 @@ function vr = endOfTraceProcedure2(vr)
     % Start the timer object   
     if (isequal(get(vr.t1, 'Running'), 'off'))
         start(vr.t1);
-        fwrite(vr.fid5, [timestampCol(1) vr.countTrials 0],'double'); %write to file that we started another trial
 
         if((double(vr.timeOfRanningInRange)/double(vr.timeOfTotalRun) >= vr.percentageThresholdOfCurrTrial/100)&&(vr.isRewardGiven == false ))
             vr = giveReward(vr); % activate reward
@@ -29,16 +27,16 @@ function vr = endOfTraceProcedure2(vr)
         else
             %go back to init position
             vr.countTrials = vr.countTrials + 1;
-            if (vr.countTrials >= vr.amountTrials)
+            if (vr.countTrials > vr.amountTrials)
                 vr.experimentEnded = true;
             else
-                vr.currentWorld = vr.stripesWorld;
+                vr.currentWorld = vr.chessWorld;
                 vr = randomizeSoundHint(vr);
                 
                 vr.position(2) = vr.initPosition;
                 scans_length = vr.ao.NotifyWhenScansQueuedBelow;
                 vr = clockAlignment(vr,scans_length); % aligment for the other sensors
-                fwrite(vr.fid5, [timestampCol(1) vr.countTrials 5],'double'); %write to file that we started another trial
+                fwrite(vr.fid5, [timestampCol(1) vr.countTrials vr.currentRewardDuration],'double'); %write to file that we started another trial
                 
                 vr.isRewardGiven = false;
                 %reset timers
--- functions/one_room_reward_by_init_sound/randomizeSoundHint.m
+++ functions/one_room_reward_by_init_sound/randomizeSoundHint.m
@@ -1,20 +1,20 @@
 function vr = randomizeSoundHint(vr)
    randomNumber = rand;
     if (randomNumber < vr.stripesWorldPercentage)
-       desiredFreq = 3200;
+       desiredFreq = 14000;
     else
-       desiredFreq = 1200;
+       desiredFreq = 8000;
     end
-    
-    vr = generateSound(vr,desiredFreq);
-    
-    % the precentage of correct running will be depended on the reward type
-    if (desiredFreq == 3200)
+        
+    if (desiredFreq == 14000)
         vr.percentageThresholdOfCurrTrial = 0;
         vr.currentRewardDuration = vr.rewardDurationBig;
     else
         vr.percentageThresholdOfCurrTrial = 0;
         vr.currentRewardDuration = vr.rewardDurationSmall;
     end
+    
+    vr = generateSound(vr,desiredFreq);
+
 end
 
